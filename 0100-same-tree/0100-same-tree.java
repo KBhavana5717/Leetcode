dd3cd19107df1f005fe184237a9f73cc3fef6ad6
KBhavana5717 <kbhavana0312@gmail.com>
2026-09-17 19:21:08 +0530

    Time: 0 ms (100%), Space: 43.1 MB (15.77%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Stack<TreeNode[]> stack = new Stack<>();
+        stack.push(new TreeNode[]{p, q});
+
+        while (!stack.isEmpty()) {
+            TreeNode[] pair = stack.pop();
+            TreeNode n1 = pair[0];
+            TreeNode n2 = pair[1];
+
+            if (n1 == null && n2 == null) continue;
+            if (n1 == null || n2 == null) return false;
+            if (n1.val != n2.val) return false;
+
+            stack.push(new TreeNode[]{n1.left, n2.left});
+            stack.push(new TreeNode[]{n1.right, n2.right});
+        }
+
+        return true;
+    }
+}
