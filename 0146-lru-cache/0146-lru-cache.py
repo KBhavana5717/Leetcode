8ae3f6e61659f9900e5731117dfaeff321a76d2a
KBhavana5717 <kbhavana0312@gmail.com>
2026-09-18 09:01:35 +0530

    Time: 19 ms (90.69%), Space: 29.5 MB (54.17%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, key=0, val=0):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {} # Map key to Node
+        
+        # Dummy head and tail to avoid edge cases during insertions/deletions
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node: Node):
+        """Remove an existing node from the linked list."""
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def _add_to_front(self, node: Node):
+        """Insert a node right after the dummy head (Most Recently Used)."""
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add_to_front(node)
+            return node.val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self._remove(node)
+            self._add_to_front(node)
+        else:
+            if len(self.cache) >= self.capacity:
+                # Evict the least recently used node (node just before tail)
+                lru = self.tail.prev
+                self._remove(lru)
+                del self.cache[lru.key]
+                
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self._add_to_front(new_node)
