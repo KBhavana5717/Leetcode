abb99ed7650571ed47165a6e8ba53fbdae48f00b
KBhavana5717 <kbhavana0312@gmail.com>
2026-09-20 19:30:50 +0530

    Time: 7 ms (97.43%), Space: 48.3 MB (30.25%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,36 @@
+class Codec {
+
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        dfsSerialize(root, sb);
+        return sb.toString();
+    }
+
+    private void dfsSerialize(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append("#,");
+            return;
+        }
+        sb.append(node.val).append(",");
+        dfsSerialize(node.left, sb);
+        dfsSerialize(node.right, sb);
+    }
+
+    public TreeNode deserialize(String data) {
+        String[] values = data.split(",");
+        int[] index = new int[1];
+        return dfsDeserialize(values, index);
+    }
+
+    private TreeNode dfsDeserialize(String[] values, int[] index) {
+        if (values[index[0]].equals("#")) {
+            index[0]++;
+            return null;
+        }
+
+        TreeNode node = new TreeNode(Integer.parseInt(values[index[0]++]));
+        node.left = dfsDeserialize(values, index);
+        node.right = dfsDeserialize(values, index);
+        return node;
+    }
+}
