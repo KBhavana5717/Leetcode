528d3e848e4a8a1a2fdfc2c6618787635c7f8bb9
KBhavana5717 <kbhavana0312@gmail.com>
2026-09-20 13:12:47 +0530

    Time: 2 ms (39.8%), Space: 19.4 MB (25.87%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        
+        while n != 1 and n not in seen:
+            seen.add(n)
+            # Compute the sum of the squares of the digits
+            n = sum(int(digit) ** 2 for digit in str(n))
+            
+        return n == 1
