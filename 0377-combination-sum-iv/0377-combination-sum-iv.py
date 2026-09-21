24cfb591ca79b3f89801aa55473284dabacad7fa
KBhavana5717 <kbhavana0312@gmail.com>
2026-09-21 10:47:47 +0530

    Time: 6 ms (96.92%), Space: 19.3 MB (68.01%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,12 @@
+class Solution:
+    def combinationSum4(self, nums: list[int], target: int) -> int:
+        # dp[i] will store the total number of combinations that add up to i
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        
+        for i in range(1, target + 1):
+            for num in nums:
+                if i - num >= 0:
+                    dp[i] += dp[i - num]
+                    
+        return dp[target]
